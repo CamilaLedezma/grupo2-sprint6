9bbfad26520c8aec1d073ef20977bc687270d300
CamilaLedezma <camila.j.ledezma@gmail.com>
2022-03-03 13:08:39 -0300

    para realizar crud de productos hay que ser Admin

--- prueba9/src/controllers/mainController.js
+++ prueba9/src/controllers/mainController.js
@@ -3,8 +3,9 @@ const path = require('path');
 let db = require('../database/models');
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
+const {validationResult} = require('express-validator');
 const imageController= require('../controllers/imageController')
-
+const User=db.User
 const Product= db.Product;
 const mainController= {
     index: async(req, res) =>{
@@ -25,11 +26,18 @@ const mainController= {
     },
     show:async(req,res)=>{
         try{
-            let products=await Product.findAll({
-                include:["discounts","images"],
-            })
+            let usuario= req.session.userLogged
+            if(usuario.roles_id==1){
+                let products=await Product.findAll({
+                    include:["discounts","images"],
+                })
                 
                 return res.render('products/productsAdmin',{products})
+            }else{
+                return res.render('yoda')
+            }
+            
+            
             
             
             
